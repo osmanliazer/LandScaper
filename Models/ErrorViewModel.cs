8d3e1b64fd20f99701964739e3c777e9b9a96188
osmanli azer <azer@osmanli.com>
2024-05-29 23:57:52 +0400

    mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LandScape.App.V1._0._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
